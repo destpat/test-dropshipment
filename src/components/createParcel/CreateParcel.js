40733ce53d5d79a5e298a9618a7b439732383909
Ficadiere Yoan <yoan.ficadiere@gmail.com>
2019-07-17 21:09:52 +0200

    feat: init project

---
+++ src/components/createParcel/CreateParcel.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CreateParcel = () => {
+  useState(() => {
+    console.log('init compoennt');
+  });
+  return <div></div>;
+};
+
+export default CreateParcel;
