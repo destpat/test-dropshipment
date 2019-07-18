037f149ad574c4b182aede0b9d24fdfe39d37f5d
Ficadiere Yoan <yoan.ficadiere@gmail.com>
2019-07-18 22:41:16 +0200

    test save

--- src/components/createParcel/CreateParcel.js
+++ src/components/createParcel/CreateParcel.js
@@ -7,8 +7,30 @@ import { items } from '../../mock/items.json';
 
 const Parcels = () => {
   useState(() => {
-    let ordersWitheItems = mergeOrdersWithItems(orders, items);
-    let parcels = splitTooHeavyOrders(ordersWitheItems);
+    const ordersWithItems = mergeOrdersWithItems(orders, items);
+    console.log(ordersWithItems);
+
+    let parcels = [];
+    ordersWithItems.forEach(order => {
+      let weight = 0;
+      let itemsToAdd = [];
+      // Trie le tableau pour avoir la plus grande valeur en premier
+      order.items.sort((a, b) => (a.weight * 1 > b.weight * 1 ? 1 : -1));
+
+      for (let i = 0; i < order.items.length; i++) {
+        weight += order.items[i].weight * 1;
+        itemsToAdd.push(order.items[i]);
+        if (weight > 30) {
+          itemsToAdd.pop();
+          parcels.push({ ...order, items: itemsToAdd });
+          itemsToAdd = [];
+          weight = 0;
+          i--;
+        }
+      }
+      parcels.push({ ...order, items: itemsToAdd });
+    });
+    console.log(parcels);
   });
   return <div></div>;
 };
