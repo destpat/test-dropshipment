58ab0eee81373092b235fead6677cb83a4d2f123
Ficadiere Yoan <yoan.ficadiere@gmail.com>
2019-07-18 08:52:23 +0200

    feat: splitting too heavy orders for create optimize parcels

--- src/components/createParcel/CreateParcel.js
+++ src/components/createParcel/CreateParcel.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-loop-func */
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import mergeOrdersWithItems from '../../utils/mergeOrdersWithItems';
+import splitTooHeavyOrders from '../../utils/splitTooHeavyOrders';
+import { orders } from '../../mock/orders.json';
+import { items } from '../../mock/items.json';
 
-const CreateParcel = () => {
+const Parcels = () => {
   useState(() => {
-    console.log('init compoennt');
+    const ordersWithItems = mergeOrdersWithItems(orders, items);
+    const parcels = splitTooHeavyOrders(ordersWithItems);
+    console.log(parcels);
   });
   return <div></div>;
 };
 
-export default CreateParcel;
+export default Parcels;
---
+++ src/utils/splitTooHeavyOrders.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-param-reassign */
+/**
+ * Découpe les commande contenant plus de 30 kilos de marchandise
+ * en plusieurs colis optimisé
+ * @param  {Object[]} orders Commande comportant le poid de chaque produit
+ * @return {Object[]}        Liste des colis
+ */
+const splitTooHeavyOrders = orders => {
+  const parcels = [];
+  orders.forEach(order => {
+    order.items.forEach(currentItem => {
+      if (currentItem.dispatched === false) {
+        let { weight } = currentItem;
+        const itemToAddInParcel = [currentItem];
+        currentItem.dispatched = true;
+
+        order.items.forEach((item, index) => {
+          if (item.dispatched === false && weight + item.weight < 30) {
+            weight += item.weight;
+            itemToAddInParcel.push(item);
+            order.items[index].dispatched = true;
+          }
+        });
+
+        parcels.push(itemToAddInParcel);
+      }
+    });
+  });
+  return parcels;
+};
+
+export default splitTooHeavyOrders;
